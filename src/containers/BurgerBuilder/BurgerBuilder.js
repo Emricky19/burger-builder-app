f4397fc0b11549f4221cee976b516e2e038fc73d
Emrick19 <emricky17@gmail.com>
2020-08-28 00:14:36 +0100

    Perfomed unit test

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -17,7 +17,7 @@ import * as actions from '../../store/actions/index'
 
 
 
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
     // constructor(props) {
     //     super(props)
     //     this.state{
@@ -90,7 +90,7 @@ class BurgerBuilder extends Component {
                 ingredients={this.props.ingredients} 
                 purchaseCancelled={this.purchaseCancelHandler}
                 purchaseContinued = {this.purchaseContinueHandler}
-                price={this.props.totalPrice.toFixed(2)}
+                price={this.props.totalPrice}
                 />
         }
 
